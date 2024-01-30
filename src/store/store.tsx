a9fa6be69b7b8ec1d79ca5a7960f509049c1756b
Vinz W <vincent.wirwicki@gmail.com>
2024-01-31 00:27:06 +0100

    small update

--- src/store/store.tsx
+++ src/store/store.tsx
@@ -1,38 +1,38 @@
 // import { ChangeEvent } from "react";
-import { proxy } from "valtio";
-import useCalcConso from "@/components/conso/calcConso/hooks/useCalcConso";
+// import { proxy } from "valtio";
+// import useCalcConso from "@/components/conso/calcConso/hooks/useCalcConso";
 
-const companyDefault = {
-  name: "",
-  params: {
-    coef: 11.18,
-    TICGN: 0.01637,
-    CTA: 34.99,
-    euroPerKwhHT: 0.0695,
-    aboHT: 16,
-  },
-  calc:useCalcConso
-};
+// const companyDefault = {
+//   name: "",
+//   params: {
+//     coef: 11.18,
+//     TICGN: 0.01637,
+//     CTA: 34.99,
+//     euroPerKwhHT: 0.0695,
+//     aboHT: 16,
+//   },
+//   calc:useCalcConso
+// };
 
-export const store = proxy({
-  companies: [{ ...companyDefault }],
-});
+// export const store = proxy({
+//   companies: [{ ...companyDefault }],
+// });
 
-export const addCompany = (
-  { m3, coef }: ConsoArgs
-) => {
-  store.companies.push({
-    name: "",
-    params: {
-      coef: 11.18,
-      TICGN: 0.01637,
-      CTA: 34.99,
-      euroPerKwhHT: 0.0695,
-      aboHT: 16,
-    },
-    calc:({},{euroPerKwhHT:},{}) => {...results}
-  });
-};
+// export const addCompany = (
+//   { m3, coef }: ConsoArgs
+// ) => {
+//   store.companies.push({
+//     name: "",
+//     params: {
+//       coef: 11.18,
+//       TICGN: 0.01637,
+//       CTA: 34.99,
+//       euroPerKwhHT: 0.0695,
+//       aboHT: 16,
+//     },
+//     calc:({},{euroPerKwhHT:},{}) => {...results}
+//   });
+// };
 
 // TICGN: { HT: 0.01637, TTC: 0 },
 // sub: { HT: 0, TTC: 0 },
